b2a9123669d21a3d3eaee69aae2f601f61c2283c
CK-LIM <80962037+CK-LIM@users.noreply.github.com>
2022-01-21 20:53:14 +0800

    Add files via upload

--- app5_ava_async_purse_tvl.py
+++ app5_ava_async_purse_tvl.py
@@ -100,6 +100,7 @@ def queryData():
     rewardPerBlock = bavaMasterFarmContract.functions.REWARD_PER_BLOCK().call()
     rewardPerBlockV1 = bavaMasterFarmContractV1.functions.REWARD_PER_BLOCK().call() 
     totalAllocPoint = bavaMasterFarmContract.functions.totalAllocPoint().call()
+    totalAllocPointV1 = bavaMasterFarmContractV1.functions.totalAllocPoint().call()
 
     for event in farm["farm"]:
         lpContract = web3.eth.contract(address=event["lpAddresses"]["43114"], abi=lpAbi["abi"])
@@ -203,7 +204,7 @@ def queryData():
             apyDaily = ""
             apyMonthly = ""
         else:
-            apr = ((28000 * 365 * 793 * event["allocPoint"] * web3.fromWei(rewardPerBlockV1, 'ether') * decimal.Decimal(BAVAPrice) ) / (tvl * totalAllocPoint)) * 100
+            apr = ((28000 * 365 * 793 * event["allocPoint"] * web3.fromWei(rewardPerBlockV1, 'ether') * decimal.Decimal(BAVAPrice) ) / (tvl * totalAllocPointV1)) * 100
             apyDaily = ((1 + apr/36500)**365 -1) * 100
             apyWeekly = ((1 + apr/5200)**52 -1) * 100
             apyMonthly = ((1 + apr/1200)**12 -1) * 100
